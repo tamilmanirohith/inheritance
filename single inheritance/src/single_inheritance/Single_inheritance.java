245d3437ccf5290a703fc0bcaa650dd00ff5d9f8
tamilmanirohith <123348635+tamilmanirohith@users.noreply.github.com>
2023-01-23 11:05:46 +0530

    Add files via upload

---
+++ single inheritance/src/single_inheritance/Single_inheritance.java
@@ -0,0 +1,21 @@
+package single_inheritance;
+
+class Employee {
+    void salary() {
+        System.out.println("Salary= 200000");
+    }
+}
+
+class Programmer extends Employee { 
+    void bonus() {
+        System.out.println("Bonus=50000");
+    }
+}
+
+class Single_inheritance {
+    public static void main(String args[]) {
+        Programmer p = new Programmer();
+        p.salary(); 
+        p.bonus(); 
+    }
+}
